dfb02e1f1169434c693674838054c7d62dea9d2f
Pascal Mpawenayo <pm@gmail.com>
2017-02-10 17:22:57 -0500

    Project Set up and first test pass

---
+++ src/main/java/io/github/pascalmp/SinkingSort.java
@@ -0,0 +1,10 @@
+package io.github.pascalmp;
+
+public class SinkingSort {
+
+	public static int[] sort(int[] is) {
+		// TODO Auto-generated method stub
+		return is;
+	}
+
+}
---
+++ src/test/java/io/github/pascalmp/SinkingSortTest.java
@@ -0,0 +1,13 @@
+package io.github.pascalmp;
+
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+
+public class SinkingSortTest {
+	
+	@Test
+	public void shouldSortSortedList(){
+		assertArrayEquals(new int[]{0, 1}, SinkingSort.sort(new int[]{0, 1}));
+	}
+
+}
